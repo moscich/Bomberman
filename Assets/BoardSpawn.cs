f2884de55f1e8ad5b732499c645f298c477c41b0
Marek Mościchowski <marek.moscichowski@codewise.com>
2017-10-29 15:39:19 +0100

    Initial

---
+++ Assets/BoardSpawn.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BoardElement {
+	solid,
+	wall,
+	empty
+};
+
+public class BoardSpawn : MonoBehaviour {
+
+	public Transform brick;
+	public Transform wall;
+
+	public Dictionary<int, BoardElement> board;
+
+	public int DoSomething() {
+		return 42;
+	}
+
+	// Use this for initialization
+	void Start () {
+		board = new Dictionary<int, BoardElement> ();
+		for (int row = 0; row < 11; row++) {
+			for (int column = 0; column < 13; column++) {
+				BoardElement element;
+				if (row % 2 == 1 && column % 2 == 1) {
+					element = BoardElement.solid;
+				} else {
+					element = BoardElement.wall;
+				}
+				board.Add (row * 13 + column, element);
+			}
+		}
+
+		board[5 * 13 + 5] = BoardElement.wall;
+
+		for (int row = 0; row < 11; row++) {
+			for (int column = 0; column < 13; column++) {
+				BoardElement element;
+				board.TryGetValue (row * 13 + column, out element);
+				Transform field = element == BoardElement.solid ? wall : brick;
+				Transform obj = Instantiate (field, new Vector3 (column, row, this.transform.position.z), Quaternion.identity);
+				obj.parent = this.transform;
+			}
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/SimpleMove.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SimpleMove : NetworkBehaviour {
+
+	public BoardSpawn board;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3 (6, 5, 0);
+		board = GameObject.Find("Board").GetComponent<BoardSpawn>();
+	}
+	
+	float speed = 1.0f;
+
+	void Update() {
+
+		if (!isLocalPlayer)
+		{
+			return;
+		}
+
+		Vector2 currentField = new Vector2 (Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y));
+
+		Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+		Vector3 destination = transform.position + (move * speed * Time.deltaTime);
+
+		int destinationFieldY;
+		if (destination.y > transform.position.y) {
+			destinationFieldY = (int)Mathf.Floor (destination.y + 1);
+		} else {
+			destinationFieldY = (int)Mathf.Floor (destination.y);
+		}
+
+		int destinationFieldX;
+		if (destination.x > transform.position.x) {
+			destinationFieldX = (int)Mathf.Floor (destination.x + 1);
+		} else {
+			destinationFieldX = (int)Mathf.Floor (destination.x);
+		}
+
+		BoardElement destinationYElement;
+		board.board.TryGetValue (13 * destinationFieldY + (int)currentField.x, out destinationYElement);
+		if (destinationYElement == BoardElement.solid) {
+			if (destination.y > transform.position.y) {
+				destination.y = Mathf.Min (destination.y, transform.position.y);
+			} else {
+				destination.y = Mathf.Max (destination.y, transform.position.y);
+			}
+		}
+
+		BoardElement destinationXElement;
+		board.board.TryGetValue (13 * (int)currentField.y + destinationFieldX, out destinationXElement);
+		if (destinationXElement == BoardElement.solid) {
+			if (destination.x > transform.position.x) {
+				destination.x = Mathf.Min (destination.x, transform.position.x);
+			} else {
+				destination.x = Mathf.Max (destination.x, transform.position.x);
+			}
+		}
+
+		destination.x = Mathf.Max (destination.x, 0);
+		destination.y = Mathf.Max (destination.y, 0);
+		transform.position = destination;
+	}
+}
